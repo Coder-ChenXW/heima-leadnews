b0e68f5dbf37b90a2bb8978ceb6e9cc78898a238
chenxuanwen <chenxw1024@163.com>
2022-07-24 18:29:37 +0800

    完成MinIO的相关代码

--- heima-leadnews-test/minio-demo/src/test/java/com/heima/minio/test/MinIOTest.java
+++ heima-leadnews-test/minio-demo/src/test/java/com/heima/minio/test/MinIOTest.java
@@ -1,26 +1,47 @@
 package com.heima.minio.test;
 
+import com.heima.file.service.FileStorageService;
+import com.heima.minio.MinIOApplication;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
+@SpringBootTest(classes = MinIOApplication.class)
+@RunWith(SpringRunner.class)
 public class MinIOTest {
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    //把list.html上传到minio中并且可以在浏览器中访问
+    /*@Test
+    public void test() throws FileNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream("D:\\list.html");
+        String path = fileStorageService.uploadHtmlFile("", "list.html", fileInputStream);
+        System.out.println(path);
+    }*/
+
+
 
     public static void main(String[] args) {
 
-        FileInputStream fileInputStream = null;
         try {
-            fileInputStream = new FileInputStream("D:\\list.html");
+            FileInputStream fileInputStream = new FileInputStream("D:\\tmp\\js\\index.js");
             //获取minio的连接信息 创建一个minio的客户端
             MinioClient minioClient = MinioClient.builder().credentials("minio", "minio123").endpoint("http://192.168.200.130:9000").build();
 
             //上传
-            //2.上传
+            //上传
             PutObjectArgs putObjectArgs = PutObjectArgs.builder()
-                    .object("list.html")//文件名
-                    .contentType("text/html")//文件类型
+                    .object("plugins/css/index.js")//文件名
+                    .contentType("text/js")//文件类型
                     .bucket("leadnews")//桶名词  与minio创建的名词一致
                     .stream(fileInputStream, fileInputStream.available(), -1) //文件流
                     .build();
@@ -28,7 +49,7 @@ public class MinIOTest {
             minioClient.putObject(putObjectArgs);
 
             //访问路径
-            System.out.println("http://192.168.200.130:9000/leadnews/list.html");
+//            System.out.println("http://192.168.200.130:9000/leadnews/list.html");
         } catch (Exception e) {
             e.printStackTrace();
         }
