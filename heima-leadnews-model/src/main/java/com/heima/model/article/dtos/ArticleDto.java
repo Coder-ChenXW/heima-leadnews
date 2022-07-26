108cd81b46df88d208bdba2c4eeff730dd0afa18
chenxuanwen <chenxw1024@163.com>
2022-07-26 18:55:47 +0800

    完成完成app文章保存部分功能

---
+++ heima-leadnews-model/src/main/java/com/heima/model/article/dtos/ArticleDto.java
@@ -0,0 +1,16 @@
+package com.heima.model.article.dtos;
+
+import com.heima.model.article.pojos.ApArticle;
+import lombok.Data;
+
+@Data
+public class ArticleDto extends ApArticle {
+
+    /** 
+     * @Function: 功能描述 文章内容
+     * @Author: ChenXW
+     * @Date: 18:08 2022/7/26
+     */
+    private String content;
+
+}
--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/ApArticleService.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/ApArticleService.java
@@ -1,6 +1,7 @@
 package com.heima.article.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.heima.model.article.dtos.ArticleDto;
 import com.heima.model.article.dtos.ArticleHomeDto;
 import com.heima.model.article.pojos.ApArticle;
 import com.heima.model.common.dtos.ResponseResult;
@@ -15,4 +16,12 @@ public interface ApArticleService extends IService<ApArticle> {
      * @Date: 20:30 2022/7/22
      */
     public ResponseResult load(ArticleHomeDto dto,Short type);
+
+
+    /** 
+     * @Function: 功能描述 保存app端相关文章
+     * @Author: ChenXW
+     * @Date: 18:41 2022/7/26
+     */
+    public ResponseResult saveArticle(ArticleDto dto);
 }
