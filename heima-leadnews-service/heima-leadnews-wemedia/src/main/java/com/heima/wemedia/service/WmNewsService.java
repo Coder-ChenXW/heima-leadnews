ee57cd1b025fb61ba2d872779b269773892a6a1a
chenxuanwen <chenxw1024@163.com>
2022-07-26 16:10:54 +0800

    完成自媒体文章发布相关功能

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/WmNewsService.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/WmNewsService.java
@@ -2,6 +2,7 @@ package com.heima.wemedia.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.wemedia.dtos.WmNewsDto;
 import com.heima.model.wemedia.dtos.WmNewsPageReqDto;
 import com.heima.model.wemedia.pojos.WmNews;
 
@@ -13,5 +14,12 @@ public interface WmNewsService extends IService<WmNews> {
      * @Date: 18:23 2022/7/25
      */
     public ResponseResult findList(WmNewsPageReqDto dto);
+
+    /** 
+     * @Function: 功能描述发布修改文章或保存为草稿
+     * @Author: ChenXW
+     * @Date: 15:17 2022/7/26
+     */
+    public ResponseResult submitNews(WmNewsDto dto);
 
 }
--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmNewsServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmNewsServiceImpl.java
@@ -7,16 +7,20 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.heima.common.constants.WemediaConstants;
 import com.heima.common.exception.CustomException;
 import com.heima.model.common.dtos.PageResponseResult;
 import com.heima.model.common.dtos.ResponseResult;
 import com.heima.model.common.enums.AppHttpCodeEnum;
+import com.heima.model.wemedia.dtos.WmNewsDto;
 import com.heima.model.wemedia.dtos.WmNewsPageReqDto;
 import com.heima.model.wemedia.pojos.WmMaterial;
 import com.heima.model.wemedia.pojos.WmNews;
+import com.heima.model.wemedia.pojos.WmNewsMaterial;
 import com.heima.utils.thread.WmThreadLocalUtil;
 import com.heima.wemedia.mapper.WmMaterialMapper;
 import com.heima.wemedia.mapper.WmNewsMapper;
+import com.heima.wemedia.mapper.WmNewsMaterialMapper;
 import com.heima.wemedia.service.WmNewsService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -87,5 +91,172 @@ public class WmNewsServiceImpl extends ServiceImpl<WmNewsMapper, WmNews> impleme
 
 
         return responseResult;
+    }
+
+    /**
+     * @param dto
+     * @Function: 功能描述发布修改文章或保存为草稿
+     * @Author: ChenXW
+     * @Date: 15:17 2022/7/26
+     */
+    @Override
+    public ResponseResult submitNews(WmNewsDto dto) {
+        //条件判断
+        if (dto == null || dto.getContent() == null) {
+            return ResponseResult.errorResult(AppHttpCodeEnum.PARAM_INVALID);
+        }
+
+        //保存或修改文章
+
+        WmNews wmNews = new WmNews();
+
+        //属性拷贝 属性名称和类型相同才能拷贝
+        BeanUtils.copyProperties(dto, wmNews);
+        //封面图片
+        if (dto.getImages() != null && dto.getImages().size() > 0) {
+            String imageStr = StringUtils.join(dto.getImages(), ",");
+            wmNews.setImages(imageStr);
+        }
+
+        //如果当前封面类型为自动
+        if (dto.getType().equals(WemediaConstants.WM_NEWS_TYPE_AUTO)) {
+            wmNews.setType(null);
+        }
+
+        saveOrUpdateWmNews(wmNews);
+
+        //判断是否为草稿,如果为草稿结束当前方法
+        if (dto.getStatus().equals(WmNews.Status.NORMAL.getCode())) {
+            return ResponseResult.okResult(AppHttpCodeEnum.SUCCESS);
+        }
+
+        //不算草稿,保存文章内容图片与素材的关系
+        //获取到文章内容中的图片信息
+        List<String> materials = ectractUrlInfo(dto.getContent());
+        saveRealativeInfoForContent(materials, wmNews.getId());
+
+        //不是草稿，保存文章封面图片与素材的关系，如果布局是自动则匹配封面图片
+        saveRealativeInfoForCover(dto, wmNews, materials);
+
+        return ResponseResult.okResult(AppHttpCodeEnum.SUCCESS);
+    }
+
+    /**
+     * @Function: 功能描述 如果布局是自动则匹配封面图片
+     * @Author: ChenXW
+     * @Date: 16:00 2022/7/26
+     */
+    private void saveRealativeInfoForCover(WmNewsDto dto, WmNews wmNews, List<String> materials) {
+
+        List<String> images = dto.getImages();
+
+        if (dto.getType().equals(WemediaConstants.WM_NEWS_TYPE_AUTO)) {
+            //多图
+            if (materials.size() >= 3) {
+                wmNews.setType(WemediaConstants.WM_NEWS_MANY_IMAGE);
+                images = materials.stream().limit(3).collect(Collectors.toList());
+            } else if (materials.size() >= 1 && materials.size() < 3) {
+                //单图
+                wmNews.setType(WemediaConstants.WM_NEWS_SINGLE_IMAGE);
+                images = materials.stream().limit(1).collect(Collectors.toList());
+            } else {
+                //无图
+                wmNews.setType(WemediaConstants.WM_NEWS_NONE_IMAGE);
+            }
+
+            //修改文章
+            if (images != null && images.size() > 0) {
+                wmNews.setImages(StringUtils.join(images, ","));
+            }
+            updateById(wmNews);
+        }
+        if (images != null && images.size() > 0) {
+            saveRealativeInfo(images, wmNews.getId(), WemediaConstants.WM_COVER_REFERENCE);
+        }
+    }
+
+    /**
+     * @Function: 功能描述 处理文章内容图片与素材的关系
+     * @Author: ChenXW
+     * @Date: 15:43 2022/7/26
+     */
+    private void saveRealativeInfoForContent(List<String> materials, Integer newsId) {
+        saveRealativeInfo(materials, newsId, WemediaConstants.WM_CONTENT_REFERENCE);
+    }
+
+    @Autowired
+    private WmMaterialMapper wmMaterialMapper;
+
+    /**
+     * @Function: 功能描述 保存文章图片与素材的关系到数据库中
+     * @Author: ChenXW
+     * @Date: 15:45 2022/7/26
+     */
+    private void saveRealativeInfo(List<String> materials, Integer newsId, Short type) {
+        if (materials != null && !materials.isEmpty()) {
+            //通过图片的url查询素材的id
+            List<WmMaterial> dbMaterials = wmMaterialMapper.selectList(Wrappers.<WmMaterial>lambdaQuery().in(WmMaterial::getUrl, materials));
+
+            //判断素材是否有效
+            if (dbMaterials == null || dbMaterials.size() == 0) {
+                //手动抛出异常
+                throw new CustomException(AppHttpCodeEnum.MATERIASL_REFERENCE_FAIL);
+            }
+
+            if (materials.size() != dbMaterials.size()) {
+                throw new CustomException(AppHttpCodeEnum.MATERIASL_REFERENCE_FAIL);
+            }
+
+            List<Integer> idList = dbMaterials.stream().map(WmMaterial::getId).collect(Collectors.toList());
+
+            //批量保存
+            wmNewsMaterialMapper.saveRelations(idList, newsId, type);
+        }
+    }
+
+    /**
+     * @Function: 功能描述 提取文章内容中的图片信息
+     * @Author: ChenXW
+     * @Date: 15:40 2022/7/26
+     */
+    private List<String> ectractUrlInfo(String content) {
+        List<String> materials = new ArrayList<>();
+
+        List<Map> maps = JSON.parseArray(content, Map.class);
+        for (Map map : maps) {
+            if (map.get("type").equals("image")) {
+                String imgUrl = (String) map.get("value");
+                materials.add(imgUrl);
+            }
+        }
+
+        return materials;
+    }
+
+
+    @Autowired
+    private WmNewsMaterialMapper wmNewsMaterialMapper;
+
+    /**
+     * @Function: 功能描述 保存或修改文章
+     * @Author: ChenXW
+     * @Date: 15:31 2022/7/26
+     */
+    private void saveOrUpdateWmNews(WmNews wmNews) {
+        //补全属性
+        wmNews.setUserId(WmThreadLocalUtil.getUser().getId());
+        wmNews.setCreatedTime(new Date());
+        wmNews.setSubmitedTime(new Date());
+        wmNews.setEnable((short) 1);//默认上架
+
+        if (wmNews.getId() == null) {
+            //保存
+            save(wmNews);
+        } else {
+            //修改
+            //删除文章图片与素材的关系
+            wmNewsMaterialMapper.delete(Wrappers.<WmNewsMaterial>lambdaQuery().eq(WmNewsMaterial::getNewsId, wmNews.getId()));
+            updateById(wmNews);
+        }
     }
 }
