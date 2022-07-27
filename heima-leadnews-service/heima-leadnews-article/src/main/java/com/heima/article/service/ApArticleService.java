14af24e7e837167bf7024708d44f6108f7d92c36
chenxuanwen <chenxw1024@163.com>
2022-07-27 11:00:31 +0800

    完成完成app文章保存

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/ApArticleService.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/ApArticleService.java
@@ -1,9 +1,11 @@
 package com.heima.article.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.heima.model.article.dtos.ArticleDto;
 import com.heima.model.article.dtos.ArticleHomeDto;
 import com.heima.model.article.pojos.ApArticle;
 import com.heima.model.common.dtos.ResponseResult;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.io.IOException;
 
@@ -15,4 +17,13 @@ public interface ApArticleService extends IService<ApArticle> {
      * @Date: 20:30 2022/7/22
      */
     public ResponseResult load(ArticleHomeDto dto,Short type);
+
+
+    /** 
+     * @Function: 功能描述 保存app端相关文章
+     * @Author: ChenXW
+     * @Date: 10:38 2022/7/27
+     */
+    public ResponseResult saveArticle(ArticleDto dto);
+
 }
